79e6f4e55263e494e88d3f566ce97eb5b54bbbf7
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-02-01 14:29:45 -0800

    more styling, adding more functionality
    Co-Authored-By: Noah Kise <noah@kisefamily.com>

--- CollectionsManager/Controllers/HomeController.cs
+++ CollectionsManager/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CollectionsManager.Models;
+using System;
 
 namespace CollectionsManager.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CollectionsManagerContext _db;
+
+        public HomeController(CollectionsManagerContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet("/")]
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
